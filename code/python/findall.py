76ae864f46e58bd8facd7f0219a505eb83593923
Johan Sveholm <jsveholm@gmail.com>
2012-04-25 17:55:28 +0900

    update

--- code/python/findall.py
+++ code/python/findall.py
@@ -1,7 +1,9 @@
 import re
-re.findall(r"[0-9]","1+2==3")
+print re.findall(r"[0-9]","1+2==3")
 #--> ['1','2','3']
-re.findall(r"[0-9][0-9]","12345")
+print re.findall(r"[0-9][0-9]","12345")
 #--> ['12','34']
-re.findall(r"[0-9]+","13 from 1 in 1776")
+print re.findall(r"[0-9]+","13 from 1 in 1776")
 #--> ['13', '1', '1776'] Maximal Munch. Don't stop early. go all the way
+print "".join(set(re.findall(r'[A-Z]','I+I=ME')))
+#--> IEM, Find all unique capital letters
